4746759b1dc16298ef30c43e182384fdb9cf6f7c
beckho <vngkgk624@gmail.com>
2022-11-14 09:22:47 +0900

    version change

--- ResourceAuth/urls.py
+++ ResourceAuth/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.decorators.csrf import csrf_exempt
 
 from ResourceAuth.views import TokenView, VerifiedView, RevokeTokenView, TestView
 
 urlpatterns = [
-    path('token/', csrf_exempt(TokenView.as_view())),
-    path('verified/', csrf_exempt(VerifiedView.as_view())),
-    path('revoke/', csrf_exempt(RevokeTokenView.as_view())),
-    path('test/', csrf_exempt(TestView.as_view()))
+    path('token/', TokenView.as_view()),
+    path('verified/', VerifiedView.as_view()),
+    path('revoke/', RevokeTokenView.as_view()),
+    path('test/', TestView.as_view())
 ]
